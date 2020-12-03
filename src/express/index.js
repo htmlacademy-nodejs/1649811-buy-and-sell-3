1ecb09d45b20c151e71b0177f4edb30b8a66af2e
Alexandr Timofeev <tim31al@gmail.com>
2020-12-03 13:14:20 +0300

    module3 task2

--- src/express/index.js
+++ src/express/index.js
@@ -2,40 +2,35 @@
 
 const express = require(`express`);
 const path = require(`path`);
-const chalk = require(`chalk`);
 
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
+const {HttpCode} = require(`../constants`);
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
 const VIEWS_DIR = `templates`;
 
-// eslint-disable-next-line no-unused-vars
-const errorHandler = (err, req, res, next) => {
-  console.error(chalk.red(err.stack));
-  res.status(500).render(`errors/500`);
-};
-
-// eslint-disable-next-line no-unused-vars
-const notFoundHandler = (req, res, next) => {
-  res.status(404).render(`errors/404`);
-};
-
 const app = express();
-
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
-
-app.set(`views`, path.resolve(__dirname, VIEWS_DIR));
-app.set(`view engine`, `pug`);
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
-app.use(notFoundHandler);
-app.use(errorHandler);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).render(`errors/404`);
+});
+
+app.use((err, req, res, _next) => {
+  console.error(err.stack);
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`);
+});
+
+app.set(`views`, path.resolve(__dirname, VIEWS_DIR));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT, () =>
-  console.log(chalk.yellow(`Принимаю соединения на порт: ${DEFAULT_PORT}`)));
+  console.log(`Принимаю соединения на порт: ${DEFAULT_PORT}`));
