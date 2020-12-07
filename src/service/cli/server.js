a7efd971753586775ef977d2ddd772737f37a9ed
Alexandr Timofeev <tim31al@gmail.com>
2020-12-07 07:01:44 +0300

    fix readFile

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -4,24 +4,20 @@ const express = require(`express`);
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 const {HttpCode, DEFAULT_PORT} = require(`../../constants`);
-const {fileExists} = require(`../../utils`);
 
 const FILE_DATA = `${__dirname}/../../../mocks.json`;
 
 const router = new express.Router();
 const offersRoute = router.get(`/offers`, async (req, res) => {
-  try {
-    let json = [];
-    if (await fileExists(FILE_DATA)) {
-      const fileContent = await fs.readFile(FILE_DATA, `utf8`);
-      if (fileContent.length) {
-        json = JSON.parse(fileContent);
-      }
-    }
-    res.send(json);
-  } catch (err) {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err.message);
-  }
+  fs.readFile(FILE_DATA, `utf8`)
+    .then((data) => {
+      const json = JSON.parse(data);
+      res.send(json);
+    })
+    .catch((err) => {
+      console.error(err.stack);
+      res.send([]);
+    });
 });
 
 const app = express();
@@ -42,11 +38,7 @@ module.exports = {
   run: async (arg) => {
     const port = Number.parseInt(arg, 10) || DEFAULT_PORT;
 
-    app.listen(port, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Ошибка при создании сервера.`, err));
-      }
-
+    app.listen(port, () => {
       return console.info(chalk.yellow(`Принимаю соединения на порт: ${port}`));
     });
   }
